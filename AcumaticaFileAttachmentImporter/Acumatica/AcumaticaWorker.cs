86122c7c8362b0a0d91f98a284a519fb4e183620
jonzey23 <chris@contou.com>
2024-01-29 14:20:58 -0800

    Create New Method to Change Endpoint

--- AcumaticaFileAttachmentImporter/Acumatica/AcumaticaWorker.cs
+++ AcumaticaFileAttachmentImporter/Acumatica/AcumaticaWorker.cs
@@ -15,6 +15,7 @@ using Acumatica.RESTClient.Api;
 using Acumatica.RESTClient.Client;
 using Acumatica.RESTClient.ContractBasedApi;
 using Acumatica.RESTClient.ContractBasedApi.Model;
+using Acumatica.RESTClient.RootApi.Model;
 using AcumaticaFilesImport.Files;
 using AcumaticaFilesImport.Files.Csv;
 using AcumaticaFilesImport.Logging;
@@ -30,19 +31,20 @@ namespace AcumaticaFilesImport.Acumatica
     // 2. Create endpoint for drawings under (Default or Manufacturing??)
     public class AcumaticaWorker
     {
-        public static void ImportFile(string siteUrl,
-                                      string userName,
-                                      string passWord,
-                                      string tenant = null,
-                                      string branch = null,
-                                      string locale = null)
+        public void ImportFile(string siteUrl, Credentials credentials, Endpoint endpoint)
         {
             var authApi = new AuthApi(siteUrl, requestInterceptor: ApiExchangeLogger.LogRequest,
                 responseInterceptor: ApiExchangeLogger.LogResponse);
 
+            var userName = credentials.Name;
+            var password = credentials.Password;
+            var tenant = credentials.Tenant = null;
+            var branch = credentials.Branch = null;
+            var locale = credentials.Locale = null;
+
             try
             {
-                authApi.LogIn(userName, passWord, tenant, branch, locale);
+                authApi.LogIn(userName, password, tenant, branch, locale);
 
                 // TODO: Add a method that switches case based on the Endpoint (DocType) and calls the PutDefaultEndpointFiles method with the correct Endpoint (DocType)
 
@@ -65,7 +67,6 @@ namespace AcumaticaFilesImport.Acumatica
             }
             finally
             {
-                //we use logout in finally block because we need to always logout, even if the request failed for some reason
                 if (authApi.TryLogout())
                 {
                     Console.WriteLine("Logged out successfully.");
@@ -99,27 +100,45 @@ namespace AcumaticaFilesImport.Acumatica
             instance.PutFile("SO/SO005207", fileName, initialData);
         }
 
-
-        private Action<IEnumerable<string>, string, byte[]> GetAttachFunction(DocType docType)
+        // TODO: Pick up here.  Need method to set endpoint in import file
+        private (EntityAPI<Entity>, Entity) SetEndpoint(Endpoint endpoint)
         {
-            Action<IEnumerable<string>, string, byte[]> action;
-            switch (docType)
+
+            switch (endpoint)
             {
                 case Endpoint.SalesOrder:
-                    action = GetApi<SalesOrderApi, SalesOrder>().PutFile;
                     break;
-                case Endpoint.ARInvoice:
-                    action = GetApi<InvoiceApi, Invoice>().PutFile;
+                case Endpoint.ARInvoice: 
                     break;
                 case Endpoint.BillOfMaterial:
-                    action = GetApi<DrawingApi, Drawing>().PutFile;
                     break;
                 default:
                     throw new DocTypeUnrecognizedException();
-            }
 
-            return action;
+            }
         }
+
+
+        //private Action<IEnumerable<string>, string, byte[]> GetAttachFunction(DocType docType)
+        //{
+        //    Action<IEnumerable<string>, string, byte[]> action;
+        //    switch (docType)
+        //    {
+        //        case Endpoint.SalesOrder:
+        //            action = GetApi<SalesOrderApi, SalesOrder>().PutFile;
+        //            break;
+        //        case Endpoint.ARInvoice:
+        //            action = GetApi<InvoiceApi, Invoice>().PutFile;
+        //            break;
+        //        case Endpoint.BillOfMaterial:
+        //            action = GetApi<DrawingApi, Drawing>().PutFile;
+        //            break;
+        //        default:
+        //            throw new DocTypeUnrecognizedException();
+        //    }
+
+        //    return action;
+        //}
 
     }
 }
